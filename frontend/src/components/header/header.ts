e83d13f0fc9640c0e5c75a0bb673816dedf0f7d1
Ritik <shrestharitik@gmail.com>
2024-07-27 22:43:44 +0545

    added toast

--- frontend/src/components/header/header.ts
+++ frontend/src/components/header/header.ts
@@ -33,7 +33,7 @@ export const navbarRender = async () => {
           e.preventDefault();
           cartStore.dispatch({ type: "RESET" });
         });
-      sidebarContent!.querySelectorAll(".plus").forEach((plus) => {
+      sidebarContent!.querySelectorAll<HTMLElement>(".plus").forEach((plus) => {
         plus.addEventListener("click", function (e) {
           e.preventDefault();
           counterStore.dispatch({
@@ -43,32 +43,39 @@ export const navbarRender = async () => {
         });
       });
 
-      sidebarContent!.querySelectorAll(".minus").forEach((minus) => {
-        minus.addEventListener("click", function (e) {
-          e.preventDefault();
-          counterStore.dispatch({
-            type: "DECREMENT",
-            payload: { id: this.dataset.prod },
+      sidebarContent!
+        .querySelectorAll<HTMLElement>(".minus")
+        .forEach((minus) => {
+          minus.addEventListener("click", function (e) {
+            e.preventDefault();
+            counterStore.dispatch({
+              type: "DECREMENT",
+              payload: { id: this.dataset.prod },
+            });
           });
         });
-      });
 
-      sidebarContent!.querySelectorAll(".remove").forEach((remove) => {
-        remove.addEventListener("click", function (e) {
-          e.preventDefault();
-          cartStore.dispatch({
-            type: "REMOVE",
-            payload: { id: this.dataset.prod },
-          });
-          counterStore.dispatch({
-            type: "DECREMENT",
-            payload: {
-              id: this.dataset.prod,
-              amount: counterStore.getState()[this.dataset.prod],
-            },
+      sidebarContent!
+        .querySelectorAll<HTMLElement>(".remove")
+        .forEach((remove) => {
+          remove.addEventListener("click", function (e) {
+            e.preventDefault();
+            const prodId = this.dataset.prod;
+            if (prodId) {
+              cartStore.dispatch({
+                type: "REMOVE",
+                payload: { id: this.dataset.prod },
+              });
+              counterStore.dispatch({
+                type: "DECREMENT",
+                payload: {
+                  id: this.dataset.prod,
+                  amount: counterStore.getState()[prodId],
+                },
+              });
+            }
           });
         });
-      });
     } else {
       sidebarContent!.innerHTML = "<h1>Cart is empty</h1>";
     }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,21 @@
+import StartToastifyInstance from "toastify-js";
 import { router } from "./router";
+import "toastify-js/src/toastify.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   router();
+  StartToastifyInstance({
+    text: "This is a toast",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
 });
